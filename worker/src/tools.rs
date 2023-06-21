96edf5f4dd9ec2d7074b3462488e9c5f06dcb7fe
Meng Xu <meng.xu.cs@uwaterloo.ca>
2023-06-21 12:10:38 -0400

    x

--- worker/src/tools.rs
+++ worker/src/tools.rs
@@ -5,7 +5,7 @@ use crate::{tool_aflpp, tool_klee};
 
 /// Provision all the tools
 pub fn provision(force: bool) -> Result<()> {
-    let mut dock = Dock::new();
+    let mut dock = Dock::new()?;
     tool_aflpp::provision(&mut dock, force)?;
     tool_klee::provision(&mut dock, force)?;
     Ok(())
--- worker/src/util_docker.rs
+++ worker/src/util_docker.rs
@@ -1,17 +1,20 @@
 use std::collections::BTreeSet;
 use std::future::Future;
+use std::io::{Read, Seek};
 use std::path::Path;
 
-use anyhow::{anyhow, bail, Result};
-use docker_api::models::ImageBuildChunk;
-use docker_api::opts::{ContainerRemoveOpts, ImageBuildOpts, ImageRemoveOpts};
-use docker_api::{Container, Docker, Image};
+use anyhow::{bail, Result};
+use bollard::container::{ListContainersOptions, RemoveContainerOptions};
+use bollard::image::{BuildImageOptions, ListImagesOptions, RemoveImageOptions};
+use bollard::Docker;
 use futures_util::StreamExt;
 use log::{debug, error, info};
+use memfile::MemFile;
+use tar::Builder;
 use tokio::runtime;
 
-const UNIX_SOCKET: &str = "/var/run/docker.sock";
-const DEFAULT_PLATFORM: &str = "linux/amd64";
+struct ImageID(String);
+struct ContainerID(String);
 
 /// Utility for waiting for async actions
 fn wait_for<F: Future>(future: F) -> F::Output {
@@ -29,18 +32,19 @@ pub struct Dock {
 
 impl Dock {
     /// Create a new Docker manager
-    pub fn new() -> Self {
-        Self {
-            docker: Docker::unix(UNIX_SOCKET),
-        }
+    pub fn new() -> Result<Self> {
+        Ok(Self {
+            docker: Docker::connect_with_socket_defaults()?,
+        })
     }
 
     /// Query an image by its tag
-    pub fn get_image(&self, tag: &str) -> Result<Option<Image>> {
+    fn get_image(&self, tag: &str) -> Result<Option<ImageID>> {
         let tag_latest = format!("{}:latest", tag);
 
         let mut candidates = BTreeSet::new();
-        for image in wait_for(self.docker.images().list(&Default::default()))? {
+        let opts = ListImagesOptions::<String>::default();
+        for image in wait_for(self.docker.list_images(Some(opts)))? {
             if image.repo_tags.contains(&tag_latest) {
                 candidates.insert(image.id);
             }
@@ -52,31 +56,31 @@ impl Dock {
         match candidates.into_iter().next() {
             None => Ok(None),
             Some(id) => {
-                let image = self.docker.images().get(id);
-                let name = image.name();
-                debug!("[docker] found image \"{}\" for tag \"{}\"", name, tag);
-                Ok(Some(image))
+                debug!("[docker] found image \"{}\" for tag \"{}\"", id, tag);
+                Ok(Some(ImageID(id)))
             }
         }
     }
 
     /// Delete an image together with its associated containers
-    pub fn del_image(&mut self, image: &Image) -> Result<()> {
-        let name = image.name().to_string();
-
+    fn del_image(&mut self, id: &ImageID) -> Result<()> {
         // delete associated containers first
         let mut candidates = vec![];
-        for container in wait_for(self.docker.containers().list(&Default::default()))? {
+        let opts = ListContainersOptions::<String> {
+            all: true,
+            ..Default::default()
+        };
+        for container in wait_for(self.docker.list_containers(Some(opts)))? {
             if let Some(cid) = container.id {
                 match (container.image, container.image_id) {
                     (None, None) => (),
-                    (Some(id), None) | (None, Some(id)) => {
-                        if id == name {
+                    (Some(id0), None) | (None, Some(id0)) => {
+                        if id0 == id.0 {
                             candidates.push(cid);
                         }
                     }
                     (Some(id1), Some(id2)) => {
-                        if id1 == name || id2 == name {
+                        if id1 == id.0 || id2 == id.0 {
                             candidates.push(cid);
                         }
                     }
@@ -84,59 +88,75 @@ impl Dock {
             }
         }
         for cid in candidates {
-            self.del_container(&self.docker.containers().get(cid))?;
+            self.del_container(&ContainerID(cid))?;
         }
 
         // delete the image
-        let opts = ImageRemoveOpts::builder().force(true).build();
-        wait_for(image.remove(&opts))?;
-        debug!("[docker] image \"{}\" deleted", name);
+        let opts = RemoveImageOptions {
+            force: true,
+            ..Default::default()
+        };
+        wait_for(self.docker.remove_image(&id.0, Some(opts), None))?;
+        debug!("[docker] image \"{}\" deleted", id.0);
         Ok(())
     }
 
     /// Delete a container, stop it first if still running
-    pub fn del_container(&mut self, container: &Container) -> Result<()> {
-        let id = container.id();
-        let opts = ContainerRemoveOpts::builder()
-            .force(true)
-            .volumes(true)
-            .link(true)
-            .build();
-        wait_for(container.remove(&opts))?;
-        debug!("[docker] container \"{}\" deleted", id);
+    fn del_container(&mut self, id: &ContainerID) -> Result<()> {
+        let opts = RemoveContainerOptions {
+            force: true,
+            v: true,
+            link: true,
+        };
+        wait_for(self.docker.remove_container(&id.0, Some(opts)))?;
+        debug!("[docker] container \"{}\" deleted", id.0);
         Ok(())
     }
 
     /// Build an image from a Dockerfile
     async fn _build_async(&mut self, path: &Path, tag: &str) -> Result<()> {
+        // context tarball
+        let tx = MemFile::create_default(tag)?;
+
+        let mut tarball = Builder::new(tx);
+        tarball.append_dir_all(".", path)?;
+        tarball.finish()?;
+        let mut tx = tarball.into_inner()?;
+
+        tx.rewind()?;
+        let mut data = vec![];
+        tx.read_to_end(&mut data)?;
+        drop(tx);
+
         // build options
-        let opts = ImageBuildOpts::builder(path)
-            .tag(tag)
-            .nocahe(true)
-            .platform(DEFAULT_PLATFORM)
-            .build();
+        let opts = BuildImageOptions {
+            t: tag,
+            nocache: true,
+            ..Default::default()
+        };
 
         // run the build
-        let images = self.docker.images();
-        let mut stream = images.build(&opts);
+        let mut stream = self.docker.build_image(opts, None, Some(data.into()));
         while let Some(frame) = stream.next().await {
             let frame = frame?;
-            match frame {
-                ImageBuildChunk::Update { stream } => {
-                    print!("{}", stream);
-                }
-                ImageBuildChunk::Error {
-                    error,
-                    error_detail,
-                } => {
-                    error!("[docker] {}: {}", error, error_detail.message);
-                }
-                ImageBuildChunk::Digest { aux } => {
-                    debug!("[docker] digest {}", aux.id);
-                }
-                ImageBuildChunk::PullStatus { status, .. } => {
-                    debug!("[docker] status {}", status);
-                }
+            if let Some(msg) = frame.stream {
+                print!("{}", msg);
+            }
+            if let Some(msg) = frame.status {
+                info!("[docker] {}", msg);
+            }
+            if let Some(msg) = frame.error {
+                error!("[docker] {}", msg);
+            }
+            if let Some(msg) = frame.error_detail {
+                error!(
+                    "[docker] {} - {}",
+                    msg.code.unwrap_or(0),
+                    msg.message.unwrap_or_default()
+                );
+            }
+            if let Some(msg) = frame.progress {
+                debug!("[docker] {}", msg);
             }
         }
 
@@ -145,17 +165,17 @@ impl Dock {
     }
 
     /// Build an image from a Dockerfile, delete or reuse previous image depending on flag
-    pub fn build(&mut self, path: &Path, tag: &str, force: bool) -> Result<Image> {
+    pub fn build(&mut self, path: &Path, tag: &str, force: bool) -> Result<()> {
         // preparation
         match self.get_image(tag)? {
             None => (),
-            Some(image) => {
+            Some(id) => {
                 if force {
                     info!("[docker] deleting image \"{}\" before building", tag);
-                    self.del_image(&image)?;
+                    self.del_image(&id)?;
                 } else {
                     info!("[docker] image \"{}\" already exists", tag);
-                    return Ok(image);
+                    return Ok(());
                 }
             }
         }
@@ -164,13 +184,14 @@ impl Dock {
         wait_for(self._build_async(path, tag))?;
 
         // confirm that we actually have the image
-        self.get_image(tag)?
-            .ok_or_else(|| anyhow!("unable to locate image \"{}\"", tag))
-    }
-}
-
-impl Default for Dock {
-    fn default() -> Self {
-        Self::new()
+        match self.get_image(tag)? {
+            None => {
+                bail!("unable to locate image \"{}\"", tag);
+            }
+            Some(id) => {
+                info!("[docker] image \"{}\" built successfully: {}", tag, id.0);
+            }
+        }
+        Ok(())
     }
 }
