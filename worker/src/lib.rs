b8ec06409fbbd510ecb32e06dda883187f8ad730
Meng Xu <meng.xu.cs@uwaterloo.ca>
2023-06-21 12:57:41 -0400

    x

--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod util_docker;
 
 pub mod tool_aflpp;
+pub mod tool_gcov;
 pub mod tool_klee;
 pub mod tools;
--- worker/src/tools.rs
+++ worker/src/tools.rs
@@ -1,11 +1,12 @@
 use anyhow::Result;
 
 use crate::util_docker::Dock;
-use crate::{tool_aflpp, tool_klee};
+use crate::{tool_aflpp, tool_gcov, tool_klee};
 
 /// Provision all the tools
 pub fn provision(force: bool) -> Result<()> {
     let mut dock = Dock::new()?;
+    tool_gcov::provision(&mut dock, force)?;
     tool_aflpp::provision(&mut dock, force)?;
     tool_klee::provision(&mut dock, force)?;
     Ok(())
