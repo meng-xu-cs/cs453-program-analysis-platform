8608f61978fa790b26cb90ffbf5a7dea95074b99
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-03-01 23:06:31 -0500

    x

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use log::error;
 
 use cs453_pap_worker::process;
@@ -11,8 +13,14 @@ fn main() {
         .init()
         .expect("unable to setup logging");
 
+    // check if we need to force provision
+    let force = match env::var_os("FORCE_PROVISION") {
+        None => false,
+        Some(v) => v.to_str().map_or(false, |v| v == "1"),
+    };
+
     // handle the command line
-    match process::provision(false) {
+    match process::provision(force) {
         Ok(()) => (),
         Err(err) => {
             error!("failed to provision tools: {}", err);
